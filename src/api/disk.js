1a542bd4897c34d92315c268203d91e6c41ce9b5
Pierre Donias <pierre.donias@gmail.com>
2017-09-25 15:31:54 +0200

    feat(self): handle add/remove VDI on VM
    See vatesfr/xo-web#2348

--- src/api/disk.js
+++ src/api/disk.js
@@ -1,12 +1,39 @@
-import {parseSize} from '../utils'
+import { parseSize } from '../utils'
+import { unauthorized } from 'xo-common/api-errors'
 
 // ===================================================================
 
-export async function create ({name, size, sr}) {
-  const vdi = await this.getXapi(sr).createVdi(parseSize(size), {
+export async function create ({ name, size, sr, vm, params }) {
+  const { resourceSet } = vm
+
+  const attach = vm !== undefined
+
+  if (attach && resourceSet != null) {
+    await this.checkResourceSetConstraints(resourceSet, this.user.id, [ sr ])
+    await this.allocateLimitsInResourceSet({ disk: size }, resourceSet)
+  } else {
+    if (!(await this.hasPermissions(this.session.get('user_id'), [ [ sr, 'administrate' ] ]))) {
+      throw unauthorized()
+    }
+  }
+
+  const xapi = this.getXapi(sr)
+  sr = this.getObject(sr, 'SR')
+
+  const vdi = await xapi.createVdi(parseSize(size), {
     name_label: name,
     sr: sr._xapiId
   })
+
+  if (attach) {
+    const { bootable, position, mode } = params
+    await xapi.attachVdiToVm(vdi.$id, vm._xapiId, {
+      bootable,
+      position,
+      readOnly: mode === 'RO'
+    })
+  }
+
   return vdi.$id
 }
 
@@ -15,11 +42,21 @@ create.description = 'create a new disk on a SR'
 create.params = {
   name: { type: 'string' },
   size: { type: ['integer', 'string'] },
-  sr: { type: 'string' }
+  sr: { type: 'string' },
+  vm: { type: 'string' },
+  params: {
+    type: 'object',
+    optional: true,
+    properties: {
+      bootable: { type: 'boolean', optional: true },
+      mode: { type: 'string', optional: true },
+      position: { type: 'string', optional: true }
+    }
+  }
 }
 
 create.resolve = {
-  sr: ['sr', 'SR', 'administrate']
+  vm: ['vm', 'VM', 'administrate']
 }
 
 // -------------------------------------------------------------------
