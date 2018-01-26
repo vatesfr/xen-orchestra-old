e822262bf5969f80e8e48f2edb5526a1c1bc993b
Julien Fontanet <julien.fontanet@isonoe.net>
2018-01-26 17:45:19 +0100

    update babel conf

--- @xen-orchestra/cron/.babelrc.js
+++ @xen-orchestra/cron/.babelrc.js
@@ -1,12 +1,25 @@
-const dependencies = require('./package').dependencies || {}
+'use strict'
 
 const NODE_ENV = process.env.NODE_ENV || 'development'
 const __PROD__ = NODE_ENV === 'production'
 const __TEST__ = NODE_ENV === 'test'
 
+const pkg = require('./package')
+
+let nodeCompat = (pkg.engines || {}).node
+if (nodeCompat === undefined) {
+  nodeCompat = '6'
+} else {
+  const trimChars = '^=>~'
+  while (trimChars.includes(nodeCompat[0])) {
+    nodeCompat = nodeCompat.slice(1)
+  }
+}
+
 module.exports = {
   comments: !__PROD__,
   ignore: __TEST__ ? undefined : [/\.spec\.js$/],
+  plugins: ['lodash'],
   presets: [
     [
       '@babel/env',
@@ -17,10 +30,10 @@ module.exports = {
         targets: __PROD__
           ? {
             browsers: '>2%',
-            node: '6',
+            node: nodeCompat,
           }
           : { node: 'current' },
-        useBuiltIns: '@babel/polyfill' in dependencies && 'usage',
+        useBuiltIns: '@babel/polyfill' in (pkg.dependencies || {}) && 'usage',
       },
     ],
     '@babel/flow',
