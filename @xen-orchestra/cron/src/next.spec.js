97b87f7507c747ade2f412f6084e80d8411e9fd4
Julien Fontanet <julien.fontanet@isonoe.net>
2018-02-01 10:41:05 +0100

    do not start on a round date to reveal more issues

--- @xen-orchestra/cron/src/next.spec.js
+++ @xen-orchestra/cron/src/next.spec.js
@@ -6,7 +6,7 @@ import { DateTime } from 'luxon'
 import next from './next'
 import parse from './parse'
 
-const N = (pattern, fromDate = '2018-01-01T00:00') =>
+const N = (pattern, fromDate = '2018-04-09T06:25') =>
   next(parse(pattern), DateTime.fromISO(fromDate, { zone: 'utc' })).toISO({
     includeOffset: false,
     suppressMilliseconds: true,
@@ -16,12 +16,12 @@ const N = (pattern, fromDate = '2018-01-01T00:00') =>
 describe('next()', () => {
   mapValues(
     {
-      minutely: ['* * * * *', '2018-01-01T00:01'],
-      hourly: ['@hourly', '2018-01-01T01:00'],
-      daily: ['@daily', '2018-01-02T00:00'],
-      monthly: ['@monthly', '2018-02-01T00:00'],
+      minutely: ['* * * * *', '2018-04-09T06:26'],
+      hourly: ['@hourly', '2018-04-09T07:00'],
+      daily: ['@daily', '2018-04-10T00:00'],
+      monthly: ['@monthly', '2018-05-01T00:00'],
       yearly: ['0 0 1 jan *', '2019-01-01T00:00'],
-      weekly: ['0 0 * * mon', '2018-01-08T00:00'],
+      weekly: ['0 0 * * mon', '2018-04-16T00:00'],
     },
     ([pattern, result], title) =>
       it(title, () => {
@@ -30,12 +30,12 @@ describe('next()', () => {
   )
 
   it('select first between month-day and week-day', () => {
-    expect(N('0 0 1 * mon')).toBe('2018-01-08T00:00')
-    expect(N('0 0 2 * mon')).toBe('2018-01-02T00:00')
+    expect(N('* * 10 * wen')).toBe('2018-04-10T00:00')
+    expect(N('* * 12 * wen')).toBe('2018-04-11T00:00')
   })
 
   it('select the last available day of a month', () => {
-    expect(N('0 0 29 feb *')).toBe('2020-02-29T00:00')
+    expect(N('* * 29 feb *')).toBe('2020-02-29T00:00')
   })
 
   it('fails when no solutions has been found', () => {
